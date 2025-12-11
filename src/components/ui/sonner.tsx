779742fb57012dbce3e14766996e16ec94ce337e
Tiago de Oliveira <quickai.automation@gmail.com>
2025-12-10 22:18:28 -0300

    Clean gh-pages branch - keep only build files

--- src/components/ui/sonner.tsx
+++
@@ -1,2 +0,0 @@
-export { Toaster, toast } from "sonner"
-
--- src/pages/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as Home } from "./Home";
-export { default as Med } from "./Med";
-export { default as Beauty } from "./Beauty";
-export { default as Auto } from "./Auto";
-export { default as NotFound } from "./NotFound";
-export { default as PostPurchaseForm } from "./PostPurchaseForm";
-
